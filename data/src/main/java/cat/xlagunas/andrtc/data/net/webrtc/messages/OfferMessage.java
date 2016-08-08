0d85ea841e244367488ac8b51917ab44565188d3
Xavier Lagunas Calpe <xlagunas@gmail.com>
2016-08-08 10:41:01 +0200

    Fixed sending and creating offer

--- data/src/main/java/cat/xlagunas/andrtc/data/net/webrtc/messages/OfferMessage.java
+++ data/src/main/java/cat/xlagunas/andrtc/data/net/webrtc/messages/OfferMessage.java
@@ -2,19 +2,21 @@ package cat.xlagunas.andrtc.data.net.webrtc.messages;
 
 import com.google.gson.annotations.SerializedName;
 
+import org.webrtc.SessionDescription;
+
 /**
  * Created by xlagunas on 4/8/16.
  */
 public class OfferMessage extends WebRTCMessage {
     @SerializedName("offer")
-    private String offer;
+    private SessionDescription offer;
 
-    public OfferMessage(String userId, String roomId, String offer) {
+    public OfferMessage(String userId, String roomId, SessionDescription offer) {
         super(userId, roomId);
         this.offer = offer;
     }
 
-    public String getOffer() {
+    public SessionDescription getOffer() {
         return offer;
     }
 }
