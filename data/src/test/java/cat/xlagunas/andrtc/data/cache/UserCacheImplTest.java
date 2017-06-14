f099af8e1ff1a585c3f66337dff78031a6f65ff1
Xavier Lagunas Calpe <xavier.lagunas@ocado.com>
2017-06-14 23:15:51 +0100

    Updated support libraries + updated project to new gradle 3

--- data/src/test/java/cat/xlagunas/andrtc/data/cache/UserCacheImplTest.java
+++ data/src/test/java/cat/xlagunas/andrtc/data/cache/UserCacheImplTest.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
