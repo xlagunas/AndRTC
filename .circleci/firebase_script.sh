264ff7ea4d0a0ba8ee6c98821744196ec36fc1f4
xlagunas <xlagunas@gmail.com>
2018-02-17 02:04:36 +0100

    Added Debug and test apk generation

--- .circleci/firebase_script.sh
+++ .circleci/firebase_script.sh
@@ -3,7 +3,7 @@ sudo gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.
 sudo gcloud config set project $GCLOUD_PROJECT_ID
 sudo gcloud firebase test android run \
       --type instrumentation \
-      --app ${CIRCLE_ARTIFACTS}app/build/outputs/apk/debug/app-debug.apk \
-      --test ${CIRCLE_ARTIFACTS}/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
+      --app ${HOME}/code/app/build/outputs/apk/debug/app-debug.apk \
+      --test ${HOME}/code/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
       --device model=Nexus6,version=21,locale=en,orientation=portrait  \
       --device model=Nexus7,version=22,locale=fr,orientation=landscape
