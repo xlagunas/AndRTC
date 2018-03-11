e7f327479590ce4413edac20ee3f023a534b16c6
xlagunas <xlagunas@gmail.com>
2018-03-11 23:32:26 +0100

    Abstracted wiremock logic from tests and added Espresso login tests

--- app/src/androidTest/java/cat/xlagunas/viv/login/LoginActivityTest.kt
+++ app/src/androidTest/java/cat/xlagunas/viv/login/LoginActivityTest.kt
@@ -1,21 +1,26 @@
 package cat.xlagunas.viv.login
 
 import android.content.Intent
+import android.support.annotation.IdRes
+import android.support.design.internal.SnackbarContentLayout
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.ViewInteraction
+import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers
-import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import android.widget.EditText
 import cat.xlagunas.viv.R
 import cat.xlagunas.viv.commons.WireMockTest
 import com.github.tomakehurst.wiremock.client.WireMock
 import junit.framework.Assert.assertTrue
-import org.hamcrest.core.StringEndsWith.endsWith
+import org.hamcrest.CoreMatchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4::class)
 class LoginActivityTest : WireMockTest() {
@@ -36,9 +41,9 @@ class LoginActivityTest : WireMockTest() {
 
         activityRule.launchActivity(Intent())
 
-        onView(ViewMatchers.withId(R.id.username_input_layout)).perform(ViewActions.typeText("aRandomValidUsername"))
-        onView(ViewMatchers.withId(R.id.password_text_input)).perform(ViewActions.typeText("aRandomValidPassword"))
-        onView(ViewMatchers.withId(R.id.login_button)).perform(ViewActions.click())
+        onEditTextWithinTilWithId(R.id.username_input_layout).perform(typeText("aRandomValidUsername"), closeSoftKeyboard())
+        onEditTextWithinTilWithId(R.id.password_text_input).perform(typeText("aRandomValidPassword"), closeSoftKeyboard())
+        onView(ViewMatchers.withId(R.id.login_button)).perform(click())
 
         assertTrue(activityRule.activity.isFinishing)
 
@@ -54,11 +59,17 @@ class LoginActivityTest : WireMockTest() {
 
         activityRule.launchActivity(Intent())
 
-        onView(ViewMatchers.withId(R.id.username_input_layout)).perform(ViewActions.typeText("aRandomInvalidUsername"))
-        onView(ViewMatchers.withId(R.id.password_text_input)).perform(ViewActions.typeText("aRandomInvalidPassword"))
-        onView(ViewMatchers.withId(R.id.login_button)).perform(ViewActions.click())
+        onEditTextWithinTilWithId(R.id.username_input_layout).perform(typeText("aRandomInvalidUsername"), closeSoftKeyboard())
+        onEditTextWithinTilWithId(R.id.password_text_input).perform(typeText("aRandomInvalidPassword"), closeSoftKeyboard())
+        onView(ViewMatchers.withId(R.id.login_button)).perform(click())
 
-        onView(ViewMatchers.withClassName(endsWith("Snackbar"))).check(matches(isDisplayed()))
+        onView(isAssignableFrom(SnackbarContentLayout::class.java)).check(matches(isDisplayed()))
 
+    }
+
+    private fun onEditTextWithinTilWithId(@IdRes textInputLayoutId: Int): ViewInteraction {
+        //Note, if you have specified an ID for the EditText that you place inside
+        //the TextInputLayout, use that instead - i.e, onView(withId(R.id.my_edit_text));
+        return onView(allOf(isDescendantOfA(withId(textInputLayoutId)), isAssignableFrom(EditText::class.java)))
     }
 }
