f6f571d8a57ebe5d78168afa1c802e7223929295
xlagunas <xlagunas@gmail.com>
2020-05-03 13:52:17 +0200

    Added extension function for snackbar in fragments

--- core/src/main/java/cat/xlagunas/core/common/FragmentExtensions.kt
+++ core/src/main/java/cat/xlagunas/core/common/FragmentExtensions.kt
@@ -1,9 +1,12 @@
 package cat.xlagunas.core.common
 
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 
 fun <T : ViewModel> Fragment.viewModel(model: Class<T>): T {
     return ViewModelProviders.of(this, viewModelProviderFactory()).get(model)
@@ -11,4 +14,9 @@ fun <T : ViewModel> Fragment.viewModel(model: Class<T>): T {
 
 fun Fragment.viewModelProviderFactory(): ViewModelProvider.Factory {
     return requireActivity().viewModelProviderFactory()
+}
+
+fun Fragment.displayMessage(message: String) {
+    val layout = requireActivity().findViewById<View>(android.R.id.content)
+    Snackbar.make(layout, message, BaseTransientBottomBar.LENGTH_LONG).show()
 }
