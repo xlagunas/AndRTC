7b8318750d28ea1ba4c734154ff1c573c829c596
Xavier Lagunas <xlagunas@gmail.com>
2026-07-19 01:55:05 +0200

    Modernising app. Migrated from DataBinding to ViewBinding to remove dependency with kapt

--- user/src/main/java/cat/xlagunas/user/register/RegisterFragment.kt
+++ user/src/main/java/cat/xlagunas/user/register/RegisterFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -26,6 +26,8 @@ class RegisterFragment : Fragment() {
     private val binding: ActivityRegisterBinding
         get() = _binding!!
 
+    private val userBinder = RegisterUserBinder()
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         registerViewModel = ViewModelProvider(this, viewModelProviderFactory())
@@ -41,17 +43,27 @@ class RegisterFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = DataBindingUtil.inflate(inflater, R.layout.activity_register, container, false)
-        binding.user = RegisterUserBinder()
+        _binding = ActivityRegisterBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        actionButton = view.findViewById(R.id.fab)
+        actionButton = binding.fab
+
+        setupBinding()
+
         actionButton.setOnClickListener {
-            registerViewModel.register(RegisterUserConverter().toUser(binding.user!!))
+            registerViewModel.register(RegisterUserConverter().toUser(userBinder))
         }
+    }
+
+    private fun setupBinding() {
+        binding.content.firstNameEt.doAfterTextChanged { userBinder.firstName = it.toString() }
+        binding.content.lastName.doAfterTextChanged { userBinder.lastName = it.toString() }
+        binding.content.email.doAfterTextChanged { userBinder.email = it.toString() }
+        binding.content.username.doAfterTextChanged { userBinder.username = it.toString() }
+        binding.content.password.doAfterTextChanged { userBinder.password = it.toString() }
     }
 
     private fun handleUserAlreadyLoggedIn(user: User?) {
@@ -65,7 +77,7 @@ class RegisterFragment : Fragment() {
             is Success -> displayMessage(
                 requireContext().getString(
                     R.string.registration_successful,
-                    binding.user?.username
+                    userBinder.username
                 )
             )
         }
--- user/src/main/java/cat/xlagunas/user/register/RegisterUserBinder.kt
+++ user/src/main/java/cat/xlagunas/user/register/RegisterUserBinder.kt
@@ -1,22 +1,9 @@
 package cat.xlagunas.user.register
 
-import androidx.databinding.BaseObservable
-import androidx.databinding.Bindable
-import cat.xlagunas.user.BR
-
-class RegisterUserBinder : BaseObservable() {
-
-    @get:Bindable
-    var firstName by bindable("", BR.firstName)
-
-    @get:Bindable
-    var lastName by bindable("", BR.lastName)
-    @get:Bindable
-    var email by bindable("", BR.email)
-
-    @get:Bindable
-    var username by bindable("", BR.username)
-
-    @get:Bindable
-    var password by bindable("", BR.password)
-}
+data class RegisterUserBinder(
+    var firstName: String = "",
+    var lastName: String = "",
+    var email: String = "",
+    var username: String = "",
+    var password: String = ""
+)
